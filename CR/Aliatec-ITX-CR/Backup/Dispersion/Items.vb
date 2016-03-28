0d1546ec36b4a1deb89e937d96fb6a22575d0432
AliaTec <vbarragan@aliatec.com.mx>
2016-03-28 10:17:31 -0600

    Incio

---
+++ CR/Aliatec-ITX-CR/Backup/Dispersion/Items.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data
+
+Public Class Items
+    Inherits DataTable
+
+    Public Sub New()
+        Me.Columns.Add("Label")
+        Me.Columns.Add("Accion")
+        Me.Columns.Add("Case")
+
+        Dim newRow As DataRow
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100014
+        'accion
+        newRow.Item("Accion") = 100014
+        'case del aspx
+        newRow.Item("Case") = 100014
+        Me.Rows.Add(newRow)
+
+      
+    End Sub
+End Class
